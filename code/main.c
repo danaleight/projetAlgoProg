7c528920b96341122e523cbd706c372da1584772
Fred <danaleight2000@gmail.com>
2021-04-18 23:11:40 +0200

    troisieme commit : exemple de lecture de fichier, je dois voir comment le fichier .csv fonctionne pour pouvoir adapter le code à celui-ci

--- code/main.c
+++ code/main.c
@@ -6,15 +6,19 @@ int main()
 {
     FILE *fic = fopen("../ressources/test.txt", "r");
     int lettre = 0;
+    int id = 0, num1 = 0, num2 = 0, num3 = 0, num4 = 0;
+    int ligneRestante = 2;
 
     if(fic == NULL)
     {
         exit(1);
     }
 
-    while (1)
+    while (ligneRestante > 0)
     {
-             
+        fscanf(fic, "%d,%d,%d,%d,%d", &id, &num1, &num2, &num3, &num4);
+        printf("id : %d, %d | %d | %d | %d\n", id, num1, num2, num3, num4);
+        ligneRestante--;
     }
     
 
