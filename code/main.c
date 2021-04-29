b7bdbb0d346951ef3be16dcee6064c16843c080b
Danaleight <danaleight2000@tutanota.com>
2021-04-29 03:26:41 +0200

    avancement de la lecture et de l'enregistrement du temps un fichier csv puis création du fichier ods avec graphique dedans

--- code/main.c
+++ code/main.c
@@ -1,12 +1,107 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
-#include "lecture.h"
+typedef struct entreeCSV //structure qui permet de stocker les différentes valeurs d'une entree dans le fichier csv
+{
+    char *lon;
+    char *lat;
+    char *nombre;
+    char *adresse;
+    char *ville;
+    char *codePostal;
+    char *hash;
+} entreeCSV;
+
+//code donné dans l'ennoncé
+char *strsep (char **stringp, const char *delim)
+{
+    char *begin, *end;
+    begin = *stringp;
+    if (begin == NULL) return NULL;
+        end = begin + strcspn (begin, delim);
+    if (end)
+    {
+        *end++ = '\0';
+        *stringp = end;
+    } 
+    else {stringp = NULL;}
+        return begin;
+}
 
 int main()
 {
-    lectureFichierCSV();
+    int limitesEntree = 0;
+    int i = 0;
+    int interval = 0;
+    entreeCSV *stockage = NULL;
+    char entree[1024];
+    char *jeton, *reste;
+    const char separateurs[] = ":,;";   
+
+    FILE *fichier = fopen("../ressources/baseDeDonnee/france.csv", "r"); // creer un pointeur fichier contenant un flux (ici un fichier)
+    FILE *classeur = fopen("../ressources/temps.csv", "w"); // creer un pointeur classeur pointant vers un flux (ici un fichier)
+    fprintf(classeur, "NombreLigne;Temps\n"); //premiere ligne du fichier temps.csv
+
+    if(fichier == NULL)
+    {
+        exit(1); //si impossible de lire le fichier le programme s'arrête et renvoie l'erreur "1"
+    }
+
+    printf("Combien de lignes : ");
+    scanf("%d", &limitesEntree);
+    printf("\nInterval entre chaque releve de temps : ");
+    scanf("%d", &interval);
+
+    int taille = limitesEntree;
+    int dureeTotale = 0;
+
+    stockage = malloc(limitesEntree * sizeof(entreeCSV));
+    clock_t debutChrono = clock();
+    while(fgets(entree, 1023, fichier) != NULL && limitesEntree> 0) //tant que la fonction n'a pas atteind la fin du fichier ou tant que on a pas atteind la limite de lecture
+    {
+        char *ligne = strdup(entree);
+        //format du fichier csv pour evite d'avoir a le lire a chaque fois : 4.9356384,46.1290477,5098,lotissement vers le plat,,L'Abergement-Clémenciat,,,01400,,5f1f6c0bdb5c901c
+
+        stockage[i].lon = strsep(&ligne,",");
+        stockage[i].lat = strsep(&ligne,",");
+        stockage[i].nombre = strsep(&ligne,",");
+        stockage[i].adresse = strsep(&ligne,",");
+        jeton = strsep(&ligne,",");
+        stockage[i].ville = strsep(&ligne,",");
+        jeton = strsep(&ligne,",");
+        jeton = strsep(&ligne,",");
+        stockage[i].codePostal = strsep(&ligne,",");
+        jeton = strsep(&ligne,",");
+        stockage[i].hash = strsep(&ligne,",");
+        
+        printf("\nentree numero : %d\ncoordonnees : %s, %s\nadresse : %s %s a %s, %s\n",i + 1, stockage[i].lon, stockage[i].lat, stockage[i].nombre, stockage[i].adresse, stockage[i].ville, stockage[i].codePostal);
+        
+        limitesEntree--;
+        i++;
+        if (i % interval == 0) //tous les "interval" nombres
+        {
+            clock_t finChrono = clock();
+            fprintf(classeur, "%d;%d\n", i, ((finChrono - debutChrono) * 1000 / CLOCKS_PER_SEC));
+        }
+    }
+    if (i % interval != 0) // relever le temps de la derniere ligne lue (par exemple je veux lire 10 lignes et releve le temps toutes les 4 entree, le programme relevera quand même le temps de l'ouverture de la derniere ligne)
+    {
+        clock_t finChrono = clock();
+        fprintf(classeur, "%d;%d\n", i, ((finChrono - debutChrono) * 1000 / CLOCKS_PER_SEC));
+    }
+    
+
+    clock_t finChrono = clock();
+    dureeTotale = (finChrono - debutChrono) * 1000 / CLOCKS_PER_SEC;
+    printf("\nDuree totale de la lecture : %d ms", dureeTotale);
+
+    fclose(classeur);
+    fclose(fichier);
+
+    free(stockage);
+
     return 0;
 }
 
