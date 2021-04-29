074a975e69b676bc990cba814ad4cc0abd8b802c
Danaleight <danaleight2000@tutanota.com>
2021-04-29 07:48:16 +0200

    version utilise pour la soutenance du projet

--- code/main.c
+++ code/main.c
@@ -14,6 +14,24 @@ typedef struct entreeCSV //structure qui permet de stocker les différentes vale
     char *hash;
 } entreeCSV;
 
+int menuGenerateur(int limite, char **tab)
+{
+    int choix = -1;
+
+    for(int i = 0 ; i < limite ; i++)
+    {
+        printf("\n%s",tab[i]);
+    }
+
+    while (choix > limite || choix < 1)
+    {
+        printf("\nQue voulez-vous faire ? ");
+        scanf("%d", &choix);
+    }
+    
+    return choix;
+}
+
 //code donné dans l'ennoncé
 char *strsep (char **stringp, const char *delim)
 {
@@ -30,17 +48,49 @@ char *strsep (char **stringp, const char *delim)
         return begin;
 }
 
+
+int plusPetit(entreeCSV *t, int taille, int i)
+{
+    int petit = 0;
+    for (i ; i < taille ; i++)
+    {
+        if (strcmp(t[petit].codePostal, t[i].codePostal))
+        {
+            petit = i;
+        }
+    }
+
+    return petit;
+}
+
+entreeCSV *triBreton(entreeCSV *t, int taille)
+{
+    entreeCSV *copieTab = t;
+    for (int i = 0 ; i < taille; i++)
+    {
+        t[i] = copieTab[plusPetit(copieTab, taille, i)];
+    }
+    
+    return t;
+}
+
 int main()
 {
+    int run = 1;
     int limitesEntree = 0;
     int i = 0;
     int interval = 0;
     entreeCSV *stockage = NULL;
+    entreeCSV *tabCopie = NULL;
+
     char entree[1024];
     char *jeton, *reste;
-    const char separateurs[] = ":,;";   
+    const char separateurs[] = ":,;"; 
 
-    FILE *fichier = fopen("../ressources/baseDeDonnee/france.csv", "r"); // creer un pointeur fichier contenant un flux (ici un fichier)
+    char *menuChoixUn[] = {"1. Afficher", "2. Tri", "3. Quitter\n"};
+    int choix = -1;
+
+    FILE *fichier = fopen("../ressources/baseDeDonnee/france2.csv", "r"); // creer un pointeur fichier contenant un flux (ici un fichier)
     FILE *classeur = fopen("../ressources/temps.csv", "w"); // creer un pointeur classeur pointant vers un flux (ici un fichier)
     fprintf(classeur, "NombreLigne;Temps\n"); //premiere ligne du fichier temps.csv
 
@@ -58,7 +108,10 @@ int main()
     int dureeTotale = 0;
 
     stockage = malloc(limitesEntree * sizeof(entreeCSV));
+    tabCopie = stockage;
+
     clock_t debutChrono = clock();
+    clock_t finChrono;
     while(fgets(entree, 1023, fichier) != NULL && limitesEntree> 0) //tant que la fonction n'a pas atteind la fin du fichier ou tant que on a pas atteind la limite de lecture
     {
         char *ligne = strdup(entree);
@@ -82,23 +135,53 @@ int main()
         i++;
         if (i % interval == 0) //tous les "interval" nombres
         {
-            clock_t finChrono = clock();
+            finChrono = clock();
             fprintf(classeur, "%d;%d\n", i, ((finChrono - debutChrono) * 1000 / CLOCKS_PER_SEC));
         }
     }
     if (i % interval != 0) // relever le temps de la derniere ligne lue (par exemple je veux lire 10 lignes et releve le temps toutes les 4 entree, le programme relevera quand même le temps de l'ouverture de la derniere ligne)
     {
-        clock_t finChrono = clock();
+        finChrono = clock();
         fprintf(classeur, "%d;%d\n", i, ((finChrono - debutChrono) * 1000 / CLOCKS_PER_SEC));
+        dureeTotale = (finChrono - debutChrono) * 1000 / CLOCKS_PER_SEC;
+        printf("\nDuree totale de la lecture : %d ms", dureeTotale);
     }
-    
-
-    clock_t finChrono = clock();
-    dureeTotale = (finChrono - debutChrono) * 1000 / CLOCKS_PER_SEC;
-    printf("\nDuree totale de la lecture : %d ms", dureeTotale);
 
     fclose(classeur);
     fclose(fichier);
+
+    while(run)
+    {
+        choix = menuGenerateur(3, menuChoixUn);
+    char *menuChoixDeux[] = {"1. Tri Breton"};
+
+        if(choix == 1)
+        {
+            for(int i = 0 ; i < taille ; i++)
+            {
+                printf("\nentree numero : %d\ncoordonnees : %s, %s\nadresse : %s %s a %s, %s\n",i + 1, stockage[i].lon, stockage[i].lat, stockage[i].nombre, stockage[i].adresse, stockage[i].ville, stockage[i].codePostal);
+            }
+        }
+
+        else if (choix == 2)
+        {
+            choix = menuGenerateur(1, menuChoixDeux);
+            debutChrono = clock();
+            if (choix == 1)
+            {
+                triBreton(stockage, taille);
+            }
+
+            finChrono = clock();
+        }
+
+        else
+        {
+            run = 0;
+        }
+        
+    }
+
 
     free(stockage);
 
